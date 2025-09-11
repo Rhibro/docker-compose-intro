a92adcc238c18a7452347e1310a81efac8d50918
Rhibro <rhibron@gmail.com>
2025-09-11 10:15:01 +0200

    multi-stag docker file tasks complete

--- api/app.js
+++ api/app.js
@@ -24,7 +24,7 @@ app.use((req, res, next) => {
 });
 
 app.get('/api', (req, res) => {
-  res.json({ message: 'Hello from the API v1.1.0!' });
+  res.json({ message: 'Hello from the API!' });
 });
 
 app.post('/api/data', async (req, res) => {
