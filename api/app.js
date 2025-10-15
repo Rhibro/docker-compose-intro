6be08d893d47bc8273fa658889a2cdbcc7e86aa5
Rhibro <rhibron@gmail.com>
2025-10-15 10:42:12 +0200

    added content to app.get root

--- api/app.js
+++ api/app.js
@@ -24,6 +24,10 @@ app.use((req, res, next) => {
   next();
 });
 
+app.get('/', (req, res) => {
+  res.json({message: 'Welcome to API root!!!'});
+});
+
 app.get('/api', (req, res) => {
   res.json({ message: 'Hello from the API!' });
 });
